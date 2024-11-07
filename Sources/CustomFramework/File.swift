20e91330386af377ff0170ca9527c72eecd7c23a
hakanor <hakanor99@gmail.com>
2024-11-07 13:04:46 +0300

    first commit

---
+++ Sources/CustomFramework/File.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  
+//
+//  Created by Hakan Or on 7.11.2024.
+//
+
+import Foundation
+
+public struct SDK {
+    public static func doSomeWork() {
+        print("Doing some work...")
+    }
+}
